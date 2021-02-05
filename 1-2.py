fc619bcd92eadb0b0ef9d3c41000e631581de961
maximum-value <78367655+maximum-value@users.noreply.github.com>
2021-02-05 14:17:47 +0300

    Add files via upload

---
+++ 1-2.py
@@ -0,0 +1,10 @@
+number = 9
+value = 3.6
+roster = ['Monday', 'Sping', 3]
+train = ('Freya', 'dog', 3)
+line = 'I love a teatre'
+
+L = [number, value, roster, train, line]
+for i in L:
+    print(f'{i} is {type(i)}')
+
